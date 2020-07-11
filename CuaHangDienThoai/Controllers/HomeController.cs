6fb1dad914cf82b48ab7d73e1fb66a3861f1cea3
LECONGSANH <9991congsanh01@gmail.com>
2020-07-11 13:37:23 +0700

    cm1
    change icon dashbroad

--- CuaHangDienThoai/Controllers/HomeController.cs
+++ CuaHangDienThoai/Controllers/HomeController.cs
@@ -62,54 +62,54 @@ namespace CuaHangMayTinh.Controllers
         {
             return View();
         }
-        private Uri RediredtUri
-        {
-            get
-            {
-                var uriBuilder = new UriBuilder(Request.Url);
-                uriBuilder.Query = null;
-                uriBuilder.Fragment = null;
-                uriBuilder.Path = Url.Action("FacebookCallback");
-                return uriBuilder.Uri;
-            }
-        }
-        [AllowAnonymous]
-        public ActionResult Facebook()
-        {
-            var fb = new FacebookClient();
+        //private Uri RediredtUri
+        //{
+        //    get
+        //    {
+        //        var uriBuilder = new UriBuilder(Request.Url);
+        //        uriBuilder.Query = null;
+        //        uriBuilder.Fragment = null;
+        //        uriBuilder.Path = Url.Action("FacebookCallback");
+        //        return uriBuilder.Uri;
+        //    }
+        //}
+//        [AllowAnonymous]
+//        public ActionResult Facebook()
+//        {
+//            var fb = new FacebookClient();
 
-            var loginUrl = fb.GetLoginUrl(new
+//            var loginUrl = fb.GetLoginUrl(new
 
-            {
-                client_id = "744492899629118",
-                client_secret = "920805a51ac4217b2ddbbf504ec275aa",
-                redirect_uri = RediredtUri.AbsoluteUri,
-                response_type = "code",
-                scope = "email"
-            });
-            return Redirect(loginUrl.AbsoluteUri);
-        }
-        public ActionResult FacebookCallback(string code)
-        {
-            var fb = new FacebookClient();
-            dynamic result = fb.Post("oauth/access_token", new
-            {
-                client_id = "744492899629118",
-                client_secret = "920805a51ac4217b2ddbbf504ec275aa",
-                redirect_uri = RediredtUri.AbsoluteUri,
-                code = code
-            });
-            var accessToken = result.access_token;
-            Session["AccessToken"] = accessToken;
-            fb.AccessToken = accessToken;
-            dynamic me = fb.Get("me?fields=link,first_name,currency,last_name,email,gender,locale,timezone,verified,picture,age_range");
-            string email = me.email;
-            TempData["email"] = me.email;
-            TempData["first_name"] = me.first_name;
-            TempData["lastname"] = me.last_name;
-            TempData["picture"] = me.picture.data.url;
-            FormsAuthentication.SetAuthCookie(email, false);
-            return RedirectToAction("Index", "Home");
-        }
+//            {
+//                client_id = "744492899629118",
+//                client_secret = "920805a51ac4217b2ddbbf504ec275aa",
+//                redirect_uri = RediredtUri.AbsoluteUri,
+//                response_type = "code",
+//                scope = "email"
+//            });
+//            return Redirect(loginUrl.AbsoluteUri);
+//        }
+//        public ActionResult FacebookCallback(string code)
+//        {
+//            var fb = new FacebookClient();
+//            dynamic result = fb.Post("oauth/access_token", new
+//            {
+//                client_id = "744492899629118",
+//                client_secret = "920805a51ac4217b2ddbbf504ec275aa",
+//                redirect_uri = RediredtUri.AbsoluteUri,
+//                code = code
+//            });
+//            var accessToken = result.access_token;
+//            Session["AccessToken"] = accessToken;
+//            fb.AccessToken = accessToken;
+//            dynamic me = fb.Get("me?fields=link,first_name,currency,last_name,email,gender,locale,timezone,verified,picture,age_range");
+//            string email = me.email;
+//            TempData["email"] = me.email;
+//            TempData["first_name"] = me.first_name;
+//            TempData["lastname"] = me.last_name;
+//            TempData["picture"] = me.picture.data.url;
+//            FormsAuthentication.SetAuthCookie(email, false);
+//            return RedirectToAction("Index", "Home");
+//        }
     }
 }
